39a66ae0096d4ffe7dd2c6e2cf8007c259047484
Azhar Munir <azharsiddiqui8540@gmail.com>
2023-11-13 23:49:34 +0500

    Azhar Munir   Kainat Alam    Aima Tanveer

---
+++ Assignment no 2  F2020266157,539,447/test_battling_knights.py
@@ -0,0 +1,38 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from battling_knights import Arena, parse_moves, update_board_with_moves, output_final_state
+
+class TestKnightArena(unittest.TestCase):
+    def setUp(self):
+        self.arena = Arena()
+
+    def test_single_move_red_knight(self):
+        moves = parse_moves(StringIO("R:S\n"))
+        update_board_with_moves(self.arena, moves)
+        output_file = "output_single_move_red_knight.json" 
+        output_final_state(self.arena, output_file)
+        
+    def test_multiple_moves(self):
+        moves = parse_moves(StringIO("R:S\nB:E\nG:N\nY:N\n"))
+        update_board_with_moves(self.arena, moves)
+        output_file = "output_multiple_moves.json"  
+        output_final_state(self.arena, output_file)
+        
+    def test_knight_battle(self):
+        moves = parse_moves(StringIO("R:S\nB:S\nG:N\nY:N\n"))
+        update_board_with_moves(self.arena, moves)
+        output_file = "output_knight_battle.json"  
+        output_final_state(self.arena, output_file)
+        
+
+    def test_invalid_moves(self):
+        moves = parse_moves(StringIO("GAME-START\nINVALID\nGAME-END\n"))
+        update_board_with_moves(self.arena, moves)
+        output_file = "output_invalid_moves.json" 
+        output_final_state(self.arena, output_file)
+       
+
+
+if __name__ == "__main__":
+    unittest.main()
